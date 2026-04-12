6e4b6b41212fb837af92a209c02f97c740b36d5a
Ankit Garg <kompatibleminds@gmail.com>
2026-04-12 20:35:54 +0530

    portal ui fix + logo + branding

--- vite.portal.config.js
+++ vite.portal.config.js
@@ -4,7 +4,7 @@ const react = require("@vitejs/plugin-react");
 
 module.exports = defineConfig({
   root: path.resolve(__dirname, "portal-react"),
-  base: "/portal-app/",
+  base: process.env.NODE_ENV === "production" ? "/portal-app/" : "/",
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, "public", "portal-app"),
